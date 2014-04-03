7d7f3f94291d10b19f90cad0c612792b76220585
greengerongg <grzx2210@163.com>
2014-04-03 23:17:59 +1100

    user spec

---
+++ test/user_spec.js
@@ -0,0 +1,15 @@
+user/**
+ * frisby.js: Twitter Example
+ * (C) 2012, Vance Lucas
+ */
+var api = require('./common/common')();
+
+api.run("Should get usr form server", function(frisby){
+
+      frisby
+      // .inspectRequest()
+      .get(api.url("/user"))
+      .expectStatus(200)
+      .toss();
+
+});
